4b78c0b29281c68a0e0754d22dc55800d5df216d
saurabhlahoti30 <128593722+saurabhlahoti30@users.noreply.github.com>
2023-03-22 16:14:22 +0530

    Add files via upload

---
+++ pattern11.java
@@ -0,0 +1,54 @@
+
+class pattern11{
+ public static void main(String args[]){
+	 
+	 
+	for (int i=1;i<=5; i++){
+		for(int k=4; k>=i; k--){
+			
+	   System.out.print( " " );
+		}
+		 
+		 for(int j=1;j<=i;j++){
+		
+			System.out.print( "*");
+		 }			
+		
+			
+			for (int m=2; m<=i; m++){
+			System.out.print("*");		
+			
+		}
+		System.out.println(" ");
+	}
+	   for(int s=1; s<=5; s++){
+		   
+		for( int k =2; k<=s; k++){
+			System.out.print(" ");
+		}
+		 
+		 for(int j=5;j>=s;j--){
+		
+			System.out.print( "*");
+			
+		 }	
+		  
+		 for(int j=4;j>=s;j--){
+			 System.out.print( "*");
+			 
+		 }
+		
+		  System.out.println( " ");
+		
+	
+	}
+	
+}
+}
+  
+	  
+        
+  
+		
+
+ 
